1e16437a4c9c35c6cd1f3855b0b8e6958b379b62
alogon10 <olien29f@yahoo.co.jp>
2022-07-07 22:41:09 +0900

    first-commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Test;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $items = DB::select('select * from tasks');
+        return view('index',['items' => $items]);
+    }
+    public function create(Request $request)
+    {   
+        $validate_rule = [
+            'content' =>'required|max:20',
+        ];
+        $item = [
+            'content' => $request->content,
+        ];
+        $this->validate($request,$validate_rule);
+        DB::insert('insert into tasks (content) values (:content)', $item);
+        return redirect('/');
+    }
+    // public function update(Request $request)
+    // {
+        // $item = [
+            // 'content' => $request->updatetext,
+            // 'id' => DB::select('select id from tasks where id=:id')
+        // ];
+        // DB::update('update tasks set content=:content where id=:id', $item);
+        // return redirect('/');
+    // }
+    // public function remove(Request $request)
+    // {
+        // $param = ['id' => $request->id];
+        // DB::delete('delete from authors where id =:id', $param);
+        // return redirect('/');
+    // }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{asset('css/reset.css')}}">
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+  <title>COACHTECH</title>
+</head>
+
+<body>
+  <div class="content">
+
+  <h1>Todo  List</h1>
+    @if (count($errors) > 0)
+    <ul>
+    @foreach ($errors->all() as $error)
+      <li>{{$error}}</li>
+    @endforeach
+    </ul>
+  @endif
+  <form action="/" method="POST">
+    @csrf
+    <input class="input" type="text" name="content" >
+    <input class="button" type="submit" value="追加" >
+  <table>
+    <tr>
+      <th>作成日</th>
+      <th>タスク名</th>
+      <th>更新</th>
+      <th>削除</th>
+    </tr>
+@foreach ($items as $item)
+    <tr>
+      <td>
+        {{$item->created_at}}
+      </td>
+      <td>
+        <input class="textbox" name="updatetext" type="text" value={{$item->content}}>
+      </td>
+      <td>
+          <input formaction="/todo/update" class="update" type="submit" value="更新" name="update">
+      </td>
+      <td>
+        <input formaction="/todo/delete" class="delete" type="submit" value="削除" name="delete">
+        </form>
+      </td>
+    </tr>
+@endforeach
+  </table>
+
+</div>
+</body>
+
+</html>
